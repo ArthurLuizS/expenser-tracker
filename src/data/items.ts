da067cc27ef249b953555c32c548da3045b2292f
Art <arthurluiz36@gmail.com>
2021-11-25 18:27:58 -0200

    Valores pré setados de uma lista para simular um DB

---
+++ src/data/items.ts
@@ -0,0 +1,12 @@
+import {Item} from '../types/Item'
+
+
+/* Arquivos que serão pré carregados na aplicação, simulando um
+DataBase */
+
+export const items : Item[]= [
+    {date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 40.8},
+    {date: new Date(2021, 9, 14), category: 'food', title: 'Subway', value: 25},
+    {date: new Date(2021, 10, 9), category: 'rent', title: 'Aluguel', value: 800},
+    {date: new Date(2021, 9, 15), category: 'salary', title: 'Salário', value: 4000},
+]
