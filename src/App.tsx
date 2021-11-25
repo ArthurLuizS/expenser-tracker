53a339762ce4ad4ce365944d7098f5610673c328
Art <arthurluiz36@gmail.com>
2021-11-25 18:36:23 -0200

    Importação dos componentes e setando os states

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import * as C from './App.styles'
+import { Category } from './types/Category'
+import { Item } from './types/Item'
+import { categories } from './data/categories'
+import { items } from './data/items'
+import * as D from './helpers/dateFilter'
+
+const App = () =>{
+
+  const [list, setList] = useState(items);
+  const [filtedList, setFiltedlist] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(D.getCurrentMonth())
+
+  return(
+    <C.Container>
+      <C.Header>
+        <C.HeaderText> Sistema Financeiro </C.HeaderText>
+      </C.Header>
+      <C.Body>
+        {/* Área de informação */}
+
+        {/* Área de inserção de itens */}
+
+        {/* Área de listagem de itens  */}
+      </C.Body>
+    </C.Container>
   );
 }
 
