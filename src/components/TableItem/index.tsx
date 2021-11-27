40fa2105f3d729f9d4bf30d87299f7733afe3dcd
Art <arthurluiz36@gmail.com>
2021-11-27 17:44:18 -0200

    listagem dos itens na tabela

---
+++ src/components/TableItem/index.tsx
@@ -0,0 +1,18 @@
+import * as C from './styles'
+import { Item } from '../../types/Item'
+import {formatDate} from '../../helpers/dateFilter'
+
+type Props = {
+    item: Item;
+}
+
+export const TableItem = ( {item} : Props) => {
+    return(
+        <C.TableLine>
+            <C.TableColumn> {formatDate(item.date)} </C.TableColumn>
+            <C.TableColumn> {item.category} </C.TableColumn>
+            <C.TableColumn> {item.title} </C.TableColumn>
+            <C.TableColumn> R$ {item.value} </C.TableColumn>
+        </C.TableLine>
+    );
+}
