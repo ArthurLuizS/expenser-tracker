147835e24ea05f8e384b5372a4bee3640632f62a
Art <arthurluiz36@gmail.com>
2021-11-30 18:16:20 -0200

    manipulaçao manual dos dados

--- src/data/items.ts
+++ src/data/items.ts
@@ -5,7 +5,7 @@ import {Item} from '../types/Item'
 DataBase */
 
 export const items : Item[]= [
-    {date: new Date(2021, 10, 15), category: 'food', title: 'McDonalds', value: 40.8},
+    {date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 40.8},
     {date: new Date(2021, 10, 14), category: 'food', title: 'Subway', value: 25},
     {date: new Date(2021, 10, 9), category: 'rent', title: 'Aluguel apt', value: 800},
     {date: new Date(2021, 10, 15), category: 'salary', title: 'Salário Tex', value: 4000},
