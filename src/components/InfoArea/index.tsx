c0dc3a28acb23f10169b6658c66741b1cb23fd4b
Art <arthurluiz36@gmail.com>
2021-12-01 17:28:33 -0200

    Recebido income e expense como prpos para o ResumeItem

--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -1,15 +1,18 @@
 
 import * as C from './styles'
 import {formatCurrentMonth} from '../../helpers/dateFilter'
-import {ResumeItem} from '../../components/ResumeItem'
+import {ResumeItem} from '../ResumeItem'
+
 
 type Props ={
     currentMonth: string;
     onMonthChange: (newMonth: string) => void;
+    income: number;
+    expense: number;
     
 }
 
-export const InfoArea = ( { currentMonth, onMonthChange} : Props) => {
+export const InfoArea = ( { currentMonth, onMonthChange, income, expense} : Props) => {
 
     let handlePrevMonth = () =>{
         let [year, month] = currentMonth.split('-');
@@ -39,7 +42,9 @@ export const InfoArea = ( { currentMonth, onMonthChange} : Props) => {
             </C.MonthArea>
 
             <C.ResumeArea>
-
+                <ResumeItem title= 'Receitas' value = {income}/>
+                <ResumeItem title= 'Despesas' value = {expense}/>
+                <ResumeItem title= 'Balanço' value = {income - expense}/>
             </C.ResumeArea>
 
         </C.Container>
