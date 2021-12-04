3bdd998f985e9a2af2a7b6fef81691f9b7bd2995
Art <arthurluiz36@gmail.com>
2021-12-04 17:45:07 -0200

    Manipulando 1 mes para dezembro 2021

--- src/data/items.ts
+++ src/data/items.ts
@@ -6,7 +6,7 @@ DataBase */
 
 export const items : Item[]= [
     {date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 40.8},
-    {date: new Date(2021, 10, 14), category: 'food', title: 'Subway', value: 25},
+    {date: new Date(2021, 11, 14), category: 'food', title: 'Subway', value: 25},
     {date: new Date(2021, 10, 9), category: 'rent', title: 'Aluguel apt', value: 800},
     {date: new Date(2021, 10, 15), category: 'salary', title: 'Salário Tex', value: 4000},
 ]
