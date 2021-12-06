dea0036109883b77f7880fe4904a7b625e1f64ce
Art <arthurluiz36@gmail.com>
2021-12-06 18:29:33 -0200

    Adicionado estilizacao

--- src/components/ItemAdd/index.tsx
+++ src/components/ItemAdd/index.tsx
@@ -43,17 +43,37 @@ export const ItemAdd = ( {onAdd} : Props) =>{
 
     return(
         <C.Container>
-            <input type="Date" value={date} onChange = {e => setDate(e.target.value)}/>
+           <C.Box>
+                <C.Label>Data</C.Label>  
+                <input type="Date" value={date} onChange = {e => setDate(e.target.value)}/>
+           </C.Box>
+           
+            <C.Box> 
+                <C.Label>Categoria</C.Label>  
+                <select name="select" value={categoria} onChange ={e => setCategoria(e.target.value)}>
+                    <option value="" selected> </option>
+                    <option value="food">Alimentação </option>
+                    <option value="rent" >Despesa</option>
+                    <option value="salary">Salário</option>
+                </select>
+            </C.Box>
+            
+           <C.Box>
+                <C.Label>Título </C.Label>
+                <input type="text" placeholder='Descrição' value ={titulo} onChange={e => setTitulo(e.target.value)} /> 
+           </C.Box>
 
-            <select name="select" value={categoria} onChange ={e => setCategoria(e.target.value)}>
-                <option value="" selected> </option>
-                <option value="food">Alimentação </option>
-                <option value="rent" >Despesa</option>
-                <option value="salary">Salário</option>
-            </select>
-            <input type="text" placeholder='titulo' value ={titulo} onChange={e => setTitulo(e.target.value)} />  
-            <input type="number" value = {valor} onChange={e => setValor(e.target.value)}/>
-            <button onClick={handleAddEvent} > Adicionar </button> 
+            <C.Box> 
+                <C.Label>Valor</C.Label>
+                <input type="number" value = {valor} placeholder = "0" step='0.01' onChange={e => setValor(e.target.value)}/>
+            </C.Box>
+
+            <C.Box>
+                <C.Label> </C.Label>
+                <button onClick={handleAddEvent} > Adicionar </button> 
+            </C.Box>
+
+            
         </C.Container>
     );
 }
--- src/components/ItemAdd/styles.ts
+++ src/components/ItemAdd/styles.ts
@@ -1,14 +1,52 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+    
     backgorund-color: #FFF;
     box-shadow: 0px 0px 5px #CCC;
     border-radius: 10px;
     padding: 20px;
     margin-top: 20px;
+    display: flex;
+    align-items: center;
 
     input{
-        
+        width: 10;
+        display: auto;
+        padding: 5px 0;
+        text-align: left;
+        margin-right: 30px;
+        box-shadow: 0px 0px 5px #CCC;
     }
+    select{
+        padding: 8px 20px;
+        text-align: left;
+        margin-right: 30px;
+        box-shadow: 0px 0px 5px #CCC;
+
+    }
+    button{
+        margin-top: 10px;
+        padding: 5px 5px;
+        margin-left: 50px;
+        background-color: #ADD8E6;
+        border-style: outset;   
+    }
+`
+
+export const Box = styled.div`
+    
+`
+
+
+export const Label = styled.div`
+
+    font-weight: bold;
+    padding: 5px 0;
+    text-align: left;
+    margin-right: 100px;
+    
+    
+   
+    
 `
